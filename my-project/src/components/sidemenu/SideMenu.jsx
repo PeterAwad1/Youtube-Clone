bcd4d66795bb618ad52ae23e20e9ffb3b9768948
Peter Awad <144742353+PeterAwad1@users.noreply.github.com>
2024-08-09 22:59:04 +0300

    Fix All problems

--- my-project/src/components/sidemenu/SideMenu.jsx
+++
@@ -1,5 +0,0 @@
-function SideMenu() {
-  return <div>SideMenu</div>;
-}
-
-export default SideMenu;
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import MainSection from "./components/mainsection/MainSection";
 import Navbar from "./components/navbar/Navbar";
 import SideMenu from "./components/sidemenu/SideMenu";
