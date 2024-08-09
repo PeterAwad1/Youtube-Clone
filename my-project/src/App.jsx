fbe58cfcd27df13f2d9ecb1ccba9c3edd8d77b52
Peter Awad <144742353+PeterAwad1@users.noreply.github.com>
2024-08-09 22:07:36 +0300

    Create Components folder

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import MainSection from "./components/mainsection/MainSection";
+import Navbar from "./components/navbar/Navbar";
+import SideMenu from "./components/sidemenu/SideMenu";
 
 function App() {
-  return <div className="flex justify-center items-start">Hello</div>;
+  return (
+    <div>
+      <Navbar />
+      <SideMenu />
+      <MainSection />
+    </div>
+  );
 }
 
 export default App;
